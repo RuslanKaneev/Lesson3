fd36cb89b276499b80f95d68c051d0b479961205
Ruslan <karuslan@inbox.ru>
2022-02-02 22:32:41 +0300

    Занятие 3

---
+++ Lesson3-03/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string PrivetMassiv = "Hello";
+
+for (int j = PrivetMassiv.Length-1; j >= 0; j--)
+{
+   Console.Write(PrivetMassiv[j]);
+   
+}
+Console.ReadLine();
